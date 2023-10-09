29417b4fbae0181d43dc77ba641a8de448726e40
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 15:30:20 -0400

    Created 2 new strategies

---
+++ Connect Four/Classes/Strategies/AggressiveStrategy.cs
@@ -0,0 +1,40 @@
+﻿using Connect_Four.Classes.GameBoard;
+using Connect_Four.Interfaces;
+
+namespace Connect_Four.Classes.Strategies;
+
+class AggressiveStrategy : IStrategy
+{
+    public short GetMove(Board board, short playerNumber)
+    {
+        for (short col = 0; col < board.Columns; col++)
+        {
+            if (!board.IsValidMove(col)) continue;
+            board.MakeMove(col, playerNumber);
+
+            if (board.HasWinner)
+            {
+                board.UndoMove(col);
+                return col;
+            }
+
+            board.UndoMove(col);
+        }
+
+        for (short col = 0; col < board.Columns; col++)
+        {
+            if (!board.IsValidMove(col)) continue;
+            board.MakeMove(col, playerNumber);
+
+            if (new DefensiveStrategy().GetMove(board, playerNumber) == col)
+            {
+                board.UndoMove(col);
+                return col;
+            }
+
+            board.UndoMove(col);
+        }
+
+        return new RandomStrategy().GetMove(board, playerNumber);
+    }
+}
---
+++ Connect Four/Classes/Strategies/DefensiveStrategy.cs
@@ -0,0 +1,28 @@
+﻿using Connect_Four.Classes.GameBoard;
+using Connect_Four.Interfaces;
+
+namespace Connect_Four.Classes.Strategies;
+
+public class DefensiveStrategy : IStrategy
+{
+    public short GetMove(Board board, short playerNumber)
+    {
+        for (short col = 0; col < board.Columns; col++)
+        {
+            if (board.IsValidMove(col))
+            {
+                board.MakeMove(col, playerNumber);
+
+                if (board.HasWinner)
+                {
+                    board.UndoMove(col);
+                    return col;
+                }
+
+                board.UndoMove(col);
+            }
+        }
+
+        return new RandomStrategy().GetMove(board, playerNumber);
+    }
+}
