6e4cb00014a3177aa89872c244f066dce5cb2fc6
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 13:21:41 -0400

    Created IPlayer interface and Player abstract class for basic blueprint of types of players

---
+++ Connect Four/Classes/Players/Player.cs
@@ -0,0 +1,17 @@
+﻿using Connect_Four.Interfaces;
+
+namespace Connect_Four.Classes.Players;
+
+public abstract class Player : IPlayer
+{
+    public string Name { get; set; }
+    public int PlayerNumber { get; set; }
+
+    protected Player(string name, int playerNumber)
+    {
+        Name = name;
+        PlayerNumber = playerNumber;
+    }
+    
+    public abstract int MakeMove(Board board);
+}
---
+++ Connect Four/Interfaces/IPlayer.cs
@@ -0,0 +1,8 @@
+﻿using Connect_Four.Classes;
+
+namespace Connect_Four.Interfaces;
+
+public interface IPlayer
+{
+    int MakeMove(Board board);
+}
