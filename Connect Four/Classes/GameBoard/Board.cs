d65b390ce109694a05d13aed5edc124ea0db6a3b
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 17:45:46 -0400

    Added some comments as reminders for later

--- Connect Four/Classes/GameBoard/Board.cs
+++ Connect Four/Classes/GameBoard/Board.cs
@@ -1,12 +1,14 @@
 ﻿namespace Connect_Four.Classes.GameBoard;
 
+//Probably replace using columns and rows with a single size number when creating new board
+//because boards should always be squares
 public class Board
 {
-    public Board(short rows, short collumns)
+    private Board(short rows, short columns)
     {
-        Cells = new short[rows, collumns];
+        Cells = new short[rows, columns];
         Rows = rows;
-        Columns = collumns;
+        Columns = columns;
     }
 
     public short[,] Cells { get; set; }
@@ -16,9 +18,9 @@ public class Board
     public bool IsFull
     {
         get {
-            for (var col = 0; col < Columns; col++)
+            for (var column = 0; column < Columns; column++)
             {
-                if (Cells[0, col] != 0) continue;
+                if (Cells[0, column] != 0) continue;
                 return false;
             }
             return true;
@@ -32,12 +34,12 @@ public class Board
             // Check rows for a winner
             for (var row = 0; row < Rows; row++)
             {
-                for (var col = 0; col < Columns - 3; col++)
+                for (var column = 0; column < Columns - 3; column++)
                 {
-                    if (Cells[row, col] != 0 &&
-                        Cells[row, col] == Cells[row, col + 1] &&
-                        Cells[row, col] == Cells[row, col + 2] &&
-                        Cells[row, col] == Cells[row, col + 3])
+                    if (Cells[row, column] != 0 &&
+                        Cells[row, column] == Cells[row, column + 1] &&
+                        Cells[row, column] == Cells[row, column + 2] &&
+                        Cells[row, column] == Cells[row, column + 3])
                     {
                         return true;
                     }
@@ -47,12 +49,12 @@ public class Board
             // Check columns for a winner
             for (var row = 0; row < Rows - 3; row++)
             {
-                for (var col = 0; col < Columns; col++)
+                for (var column = 0; column < Columns; column++)
                 {
-                    if (Cells[row, col] != 0 &&
-                        Cells[row, col] == Cells[row + 1, col] &&
-                        Cells[row, col] == Cells[row + 2, col] &&
-                        Cells[row, col] == Cells[row + 3, col])
+                    if (Cells[row, column] != 0 &&
+                        Cells[row, column] == Cells[row + 1, column] &&
+                        Cells[row, column] == Cells[row + 2, column] &&
+                        Cells[row, column] == Cells[row + 3, column])
                     {
                         return true;
                     }
@@ -62,12 +64,12 @@ public class Board
             // Check diagonals for a winner
             for (var row = 0; row < Rows - 3; row++)
             {
-                for (var col = 0; col < Columns - 3; col++)
+                for (var column = 0; column < Columns - 3; column++)
                 {
-                    if (Cells[row, col] != 0 &&
-                        Cells[row, col] == Cells[row + 1, col + 1] &&
-                        Cells[row, col] == Cells[row + 2, col + 2] &&
-                        Cells[row, col] == Cells[row + 3, col + 3])
+                    if (Cells[row, column] != 0 &&
+                        Cells[row, column] == Cells[row + 1, column + 1] &&
+                        Cells[row, column] == Cells[row + 2, column + 2] &&
+                        Cells[row, column] == Cells[row + 3, column + 3])
                     {
                         return true;
                     }
@@ -92,11 +94,8 @@ public class Board
         }
     }
     
-    public bool IsValidMove(int col)
-    {
-        return col >= 0 && col < Columns && Cells[0, col] == 0;
-    }
-    
+    public bool IsValidMove(int col) => col >= 0 && col < Columns && Cells[0, col] is 0;
+
     public void MakeMove(short col, short player)
     {
         for (var row = Rows - 1; row >= 0; row--)
@@ -148,6 +147,8 @@ public class Board
 
     public static Board Create(BoardSize boardSize) => boardSize switch
     {
+        //Needs to be worked on because connecting 4 still works on larger sizes
+        //however if it is 8x8, 8 matches should be required
         BoardSize.Normal => new Board(4, 4),
         BoardSize.Large => new Board(8, 8),
         _ => throw new ArgumentOutOfRangeException(nameof(boardSize), boardSize, null)
