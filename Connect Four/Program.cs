e289c1fb320b4c0a5c7e2a607b2ae03d4a981464
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 15:15:35 -0400

    Moved functionality for starting a game to static method in Game class

--- Connect Four/Program.cs
+++ Connect Four/Program.cs
@@ -10,29 +10,6 @@ internal static class ConnectFour
     public static void Main()
     {
         Title = "Connect 4";
-        do
-        { 
-            Clear();
-            WriteLine("Welcome to Connect Four!\n");
-            var singlePlayer = false;
-            var askForGameMode = true;
-            while (askForGameMode)
-            {
-                WriteLine("Please enter '1' for single player, \n" +
-                          "enter '2' for multiplayer");
-                switch (ReadLine())
-                {
-                    case "1":
-                        singlePlayer = true;
-                        askForGameMode = false;
-                        break;
-                    case "2":
-                        singlePlayer = false;
-                        askForGameMode = false;
-                        break;
-                }
-            }
-            new Game(singlePlayer).Play();
-        } while (ShouldRestart);
+        do { Game.Start(); } while (ShouldRestart);
     }
 }
