773eeadc18b30328a619a19044fcc62a4bfdcec6
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 13:21:59 -0400

    Created basic functionality for human and computer players

---
+++ Connect Four/Classes/Players/ComputerPlayer.cs
@@ -0,0 +1,18 @@
+﻿using Connect_Four.Interfaces;
+
+namespace Connect_Four.Classes.Players;
+
+public class ComputerPlayer : Player
+{
+    public IStrategy Strategy { get; }
+
+    public ComputerPlayer(string name, int playerNumber, IStrategy strategy) : base(name, playerNumber)
+    {
+        Strategy = strategy;
+    }
+
+    public override int MakeMove(Board board)
+    {
+        return Strategy.GetMove(board, PlayerNumber);
+    }
+}
---
+++ Connect Four/Classes/Players/HumanPlayer.cs
@@ -0,0 +1,33 @@
+﻿using Connect_Four.Interfaces;
+using static System.Console;
+
+namespace Connect_Four.Classes.Players;
+
+public class HumanPlayer : Player
+{
+    public HumanPlayer(string name, int playerNumber) : base(name, playerNumber)
+    {
+    }
+    
+    public override int MakeMove(Board board)
+    {
+        while (true)
+        {
+            Write($"{Name}, enter a column number: ");
+            var input = ReadLine();
+
+            if (!int.TryParse(input, out var column))
+            {
+                WriteLine("Invalid input. Please enter a number.");
+            }
+            else if (!board.IsValidMove(column))
+            {
+                WriteLine("Invalid move. Please choose a different column.");
+            }
+            else
+            {
+                return column;
+            }
+        }
+    }
+}
