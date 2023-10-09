85de52bbd84b02234dc7bf4fd3d2eaa1f1c39909
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 13:21:06 -0400

    Created basic random strategy

---
+++ Connect Four/Classes/Strategies/RandomStrategy.cs
@@ -0,0 +1,21 @@
+﻿using Connect_Four.Interfaces;
+
+namespace Connect_Four.Classes.Strategies;
+
+public class RandomStrategy : IStrategy
+{
+    public Random Random { get; } = new();
+
+    public int GetMove(Board board, int playerNumber)
+    {
+        while (true)
+        {
+            var column = Random.Next(board.Columns);
+
+            if (board.IsValidMove(column))
+            {
+                return column;
+            }
+        }
+    }
+}
