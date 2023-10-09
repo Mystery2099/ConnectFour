030b76152577d59fa4c8efc4bc79ff5afe387e10
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-09 14:57:27 -0400

    Made Program.cs actually use a class and added some basic functionality to allow it to restart the game

--- Connect Four/Program.cs
+++ Connect Four/Program.cs
@@ -1,5 +1,38 @@
 ﻿using Connect_Four.Classes;
+using static System.Console;
 
-var game = new Game(true);
-game.Play();
-Console.ReadKey();
+namespace Connect_Four;
+
+internal static class ConnectFour
+{
+    public static bool ShouldRestart = false;
+
+    public static void Main()
+    {
+        Title = "Connect 4";
+        do
+        { 
+            Clear();
+            WriteLine("Welcome to Connect Four!\n");
+            var singlePlayer = false;
+            var askForGameMode = true;
+            while (askForGameMode)
+            {
+                WriteLine("Please enter '1' for single player, \n" +
+                          "enter '2' for multiplayer");
+                switch (ReadLine())
+                {
+                    case "1":
+                        singlePlayer = true;
+                        askForGameMode = false;
+                        break;
+                    case "2":
+                        singlePlayer = false;
+                        askForGameMode = false;
+                        break;
+                }
+            }
+            new Game(singlePlayer).Play();
+        } while (ShouldRestart);
+    }
+}
