bab602642869bc696423f7a73126c84d726f2f9e
Mathew Kennedy-Brewer <xxmysteryxxftb@gmail.com>
2023-11-10 16:50:26 -0500

    No longer setting title in program class

--- Connect Four/Program.cs
+++ Connect Four/Program.cs
@@ -1,8 +1,7 @@
 ﻿using Connect_Four.Classes.Game;
 
-Console.Title = "Connect 4";
-
 /*
  * starts the game once, then checks the static ShouldRestart property to know if it should start the game again
  */
 do { Game.Start(); } while (Game.ShouldRestartProgram);
+
